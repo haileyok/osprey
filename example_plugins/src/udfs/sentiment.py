674ddf4548b3241f6f7038e5ee548e112c5fcb14
Hailey <me@haileyok.com>
2025-12-19 15:34:21 -0800

    more work on sentiment

--- example_plugins/src/udfs/sentiment.py
+++ example_plugins/src/udfs/sentiment.py
@@ -9,16 +9,6 @@ from osprey.engine.udf.arguments import ArgumentsBase
 from osprey.engine.udf.base import UDFBase
 from osprey.worker.lib.singletons import CONFIG
 
-# Create a session with a larger connection pool for async requests
-_sentiment_session = requests.Session()
-adapter = requests.adapters.HTTPAdapter(
-    pool_connections=100,
-    pool_maxsize=100,
-    max_retries=3
-)
-_sentiment_session.mount('http://', adapter)
-_sentiment_session.mount('https://', adapter)
-
 
 class AnalyzeSentimentArguments(ArgumentsBase):
     text: str
@@ -58,7 +48,7 @@ class NeutralSentimentScoreCAF(CustomExtractedFeature[float]):
 
     @classmethod
     def feature_name(cls) -> str:
-        return 'negative_sentiment_score'
+        return 'neutral_sentiment_score'
 
     def get_serializable_feature(self) -> float | None:
         return self.score
@@ -111,7 +101,7 @@ class AnalyzeSentiment(UDFBase[AnalyzeSentimentArguments, None]):
             if not v:
                 return
 
-        response = _sentiment_session.post(self.analyze_endpoint, json={'text': arguments.text})
+        response = requests.post(self.analyze_endpoint, json={'text': arguments.text})
         response.raise_for_status()
 
         json = response.json()
